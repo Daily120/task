77e4c777ce52d469c700f291ba0549a88545a257
Daily120 <forzhacker@mail.ru>
2018-11-25 19:31:30 +0100

    Updates

---
+++ precache-manifest.d3bea397503cecc97ccd9c367a989ac5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "1bbc0f083e22de8fac89",
+    "url": "/test/static/js/main.1bbc0f08.chunk.js"
+  },
+  {
+    "revision": "858c9a79494bfccc0c9d",
+    "url": "/test/static/js/1.858c9a79.chunk.js"
+  },
+  {
+    "revision": "1bbc0f083e22de8fac89",
+    "url": "/test/static/css/main.9b77f11b.chunk.css"
+  },
+  {
+    "revision": "6e9dd4758420ba29a6fbc030298742c2",
+    "url": "/test/index.html"
+  }
+];
